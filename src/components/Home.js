59ae109f39c6b462aa88e9af1da4858e3a3c1f25
Shi Zhang, Yan <yanshizhang@gmail.com>
2022-03-10 17:50:30 -0500

    Header, section added

---
+++ src/components/Home.js
@@ -0,0 +1,24 @@
+import React from "react";
+import styled from "styled-components";
+import Section from "./Section";
+function Home() {
+  return (
+    <Container>
+      <Section
+        title="Model S"
+        description="Order Online for Touchless Delivery"
+        backgroundImg="model-s.jpg"
+        leftBtnText="Custom order"
+        rightBtnText="Existing inventory"
+      />
+      {/* <Section />
+      <Section />
+      <Section /> */}
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  height: 100vh;
+`;
+export default Home;
